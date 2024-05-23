25d9d08471f9cf46bfc3429adc73475a0184845d
navneet <navneetpalgithub@gmail.com>
2024-05-23 11:37:34 +0530

    Pluto

--- app/src/main/java/com/example/mediastore/MediaStoreUtil.kt
+++ app/src/main/java/com/example/mediastore/MediaStoreUtil.kt
@@ -3,10 +3,12 @@ package com.example.mediastore
 import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.File
 
 
 class MediaStoreUtil(
@@ -26,7 +28,7 @@ class MediaStoreUtil(
 
             val imageContentValues = ContentValues().apply {
                 put(
-                    MediaStore.Images.Media.RELATIVE_PATH,
+                    MediaStore.MediaColumns.RELATIVE_PATH,
                     Environment.DIRECTORY_PICTURES
                     )
                 put(
@@ -51,7 +53,165 @@ class MediaStoreUtil(
                 imageCollection,imageContentValues
             )
 
-            imageMediaStoreUri?.let {
+            imageMediaStoreUri?.let {uri ->
+                try {
+                    resolver.openOutputStream(uri)?.let {outputStream ->
+                        bitmap.compress(
+                            Bitmap.CompressFormat.JPEG,100,outputStream
+                        )
+
+                    }
+
+                    imageContentValues.clear()
+                    imageContentValues.put(
+                        MediaStore.Images.Media.IS_PENDING,0
+                    )
+
+                    //Refresh the gallary
+                    resolver.update(
+                        uri,imageContentValues,null,null
+                    )
+
+                }catch (e : Exception){
+                    e.printStackTrace()
+                    resolver.delete(uri,null,null)
+                }
+
+            }
+
+
+        }
+    }
+    suspend fun saveVideo(file : File){
+        //Should be run in the IO thread
+        withContext(Dispatchers.IO){
+            val resolver = context.contentResolver
+
+            val videoCollection = MediaStore.Video.Media.getContentUri(
+                MediaStore.VOLUME_EXTERNAL_PRIMARY,
+            )
+            val timeInMillis = System.currentTimeMillis()
+
+            val videoContentValues = ContentValues().apply {
+                put(
+                    MediaStore.MediaColumns.RELATIVE_PATH,
+                    Environment.DIRECTORY_MOVIES
+                    )
+                put(
+                    MediaStore.Video.Media.DISPLAY_NAME,
+                    "${timeInMillis}_video"
+                )
+                put(
+                    MediaStore.Video.Media.MIME_TYPE,
+                    "video/mp4"
+                )
+                put(
+                    MediaStore.Video.Media.DATE_ADDED,
+                    timeInMillis
+                )
+                put(
+                    MediaStore.Video.Media.IS_PENDING,
+                    1
+                )
+            }
+
+            val videoMediaStoreUri = resolver.insert(
+                videoCollection,videoContentValues
+            )
+
+            videoMediaStoreUri?.let { uri ->
+                try {
+                    //OutPut Stream
+                    resolver.openOutputStream(uri)?.use {outputStream ->
+                        resolver.openInputStream(
+                            Uri.fromFile(file)
+                        )?.use { inputStream ->
+                            inputStream.copyTo(outputStream)
+                        }
+                    }
+
+                    videoContentValues.clear()
+                    videoContentValues.put(
+                        MediaStore.Images.Media.IS_PENDING,0
+                    )
+
+                    //Refresh the gallary
+                    resolver.update(
+                        uri,videoContentValues,null,null
+                    )
+
+                }catch (e : Exception){
+                    e.printStackTrace()
+                    resolver.delete(uri,null,null)
+                }
+
+            }
+
+
+        }
+    }
+    suspend fun saveAudio(file : File){
+        //Should be run in the IO thread
+        withContext(Dispatchers.IO){
+            val resolver = context.contentResolver
+
+            val audioCollection = MediaStore.Audio.Media.getContentUri(
+                MediaStore.VOLUME_EXTERNAL_PRIMARY,
+            )
+            val timeInMillis = System.currentTimeMillis()
+
+            val audioContentValues = ContentValues().apply {
+                put(
+                    MediaStore.MediaColumns.RELATIVE_PATH,
+                    Environment.DIRECTORY_MUSIC
+                    )
+                put(
+                    MediaStore.Video.Media.DISPLAY_NAME,
+                    "${timeInMillis}_song"
+                )
+                put(
+                    MediaStore.Video.Media.MIME_TYPE,
+                    "audio/mpeg"
+                )
+                put(
+                    MediaStore.Video.Media.DATE_ADDED,
+                    timeInMillis
+                )
+                put(
+                    MediaStore.Video.Media.IS_PENDING,
+                    1
+                )
+            }
+
+            val audioMediaStoreUri = resolver.insert(
+                audioCollection,audioContentValues
+            )
+
+            audioMediaStoreUri?.let { uri ->
+                try {
+                    //OutPut Stream
+                    resolver.openOutputStream(uri)?.use {outputStream ->
+                        resolver.openInputStream(
+                            Uri.fromFile(file)
+                        )?.use { inputStream ->
+                            inputStream.copyTo(outputStream)
+                        }
+                    }
+
+                    audioContentValues.clear()
+                    audioContentValues.put(
+                        MediaStore.Images.Media.IS_PENDING,0
+                    )
+
+                    //Refresh the gallary
+                    resolver.update(
+                        uri,audioContentValues,null,null
+                    )
+
+                }catch (e : Exception){
+                    e.printStackTrace()
+                    resolver.delete(uri,null,null)
+                }
 
             }
 
