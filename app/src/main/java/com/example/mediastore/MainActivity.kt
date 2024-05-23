a2914ddd60d76763013936b3a18f1d992f41ff56
navneet <navneetpalgithub@gmail.com>
2024-05-23 13:16:46 +0530

    Pluto

--- app/src/main/java/com/example/mediastore/MainActivity.kt
+++ app/src/main/java/com/example/mediastore/MainActivity.kt
@@ -2,15 +2,15 @@ package com.example.mediastore
 
 import android.graphics.Color
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.mediastore.ui.theme.MediaStoreTheme
+import kotlinx.coroutines.runBlocking
 
 
-class MainActivity : ComponentActivity(){
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge(
@@ -20,6 +20,12 @@ class MainActivity : ComponentActivity(){
             )
         )
         super.onCreate(savedInstanceState)
+
+        val mediaStoreUtil = MediaStoreUtil(this)
+        val file = mediaStoreUtil.getRawAudioFile(R.raw.datschenma)
+        runBlocking {
+            mediaStoreUtil.saveAudio(file)
+        }
         setContent {
             MediaStoreTheme {
 
